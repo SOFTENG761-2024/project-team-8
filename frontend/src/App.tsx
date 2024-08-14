e4e4ff5fede494608678f031408f12b42d7852fe
Amisha Singh <amishas171@gmail.com>
2024-08-14 17:28:44 +1200

    chore: initalised husky pre commit hooks to run prettier to format code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-import { Router } from "./Router";
-import { theme } from "./theme";
+import '@mantine/core/styles.css'
+import { MantineProvider } from '@mantine/core'
+import { Router } from './Router'
+import { theme } from './theme'
 
 function App() {
   return (
     <MantineProvider theme={theme}>
       <Router />
     </MantineProvider>
-  );
+  )
 }
 
-export default App;
+export default App
