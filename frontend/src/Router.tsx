6d95bd6fe9f1158d074d440476308a0370ef4a04
Atharva Arankalle <atharvanz@gmail.com>
2024-08-28 22:11:36 +1200

    ✨ feat(pages): added pages common to teachers and administrator users

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,10 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import DashboardPage from "./pages/Dashboard.page";
+import LoginPage from "./pages/Login.page";
+import RegisterPage from "./pages/Register.page";
+import CoursePage from "./pages/Course.page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <DashboardPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/courses/:courseId",
+    element: <CoursePage />,
   },
 ]);
 
