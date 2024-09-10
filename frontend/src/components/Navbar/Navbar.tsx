9f07f56b6617280ff0e5ea53fd4c7c7d31c59229
Jolin <jche521@aucklanduni.ac.nz>
2024-09-10 13:09:48 +1200

    ✨ feat(navbar folder): expandable navbar look (with no toggle button)

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,43 +1,62 @@
-import { ActionIcon, rem, Stack, Tooltip } from "@mantine/core";
+import { ActionIcon, rem, Stack, Tooltip, Text } from "@mantine/core";
 import classes from "./NavBar.module.css";
 import { IconHomeFilled } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 interface NavbarLinkProps {
   icon: typeof IconHomeFilled;
   label: string;
   active?: boolean;
+  expanded?: boolean;
   onClick?: () => void;
 }
 
-function NavbarLink({ icon: Icon, label, active, onClick }: NavbarLinkProps) {
+function NavbarLink({
+  icon: Icon,
+  label,
+  active,
+  onClick,
+  expanded = true,
+}: NavbarLinkProps) {
   const iconSize = rem("calc(1.5vw + 4px)");
 
   return (
-    <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+    <div className={classes.link}>
       <ActionIcon
         onClick={onClick}
         variant="transparent"
-        className={classes.link}
         data-active={active || undefined}
+        className={classes.linkIcon}
       >
         <Icon style={{ width: iconSize, height: iconSize }} stroke={3} />
       </ActionIcon>
-    </Tooltip>
+      {expanded && <Text>{label}</Text>}
+    </div>
   );
 }
 
 const Navbar = () => {
   const navigate = useNavigate();
+
+  const [expanded, setExpanded] = useState<boolean>(true);
+
   const links = [{ icon: IconHomeFilled, label: "Dashboard", path: "/" }];
   const navbarItems = links.map((link) => (
-    <NavbarLink key={link.label} {...link} onClick={() => navigate(link.path)} />
+    <NavbarLink
+      key={link.label}
+      {...link}
+      onClick={() => navigate(link.path)}
+      expanded={expanded}
+    />
   ));
   return (
-    <nav className={classes.navbar}>
-      <div className={classes.navbarMain}>
-        <Stack gap="2vh">{navbarItems}</Stack>
-      </div>
+    <nav
+      className={`${classes.navbar} ${expanded ? classes.navbarExpanded : ""}`}
+    >
+      <Stack gap="2vh" className={classes.navbarMain}>
+        {navbarItems}
+      </Stack>
     </nav>
   );
 };
