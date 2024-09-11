39604205f75213fc03c072fed6a6a3452af6ceb0
Tuan <tle428@aucklanduni.ac.nz>
2024-09-11 20:22:17 +1200

    feat-applied logo and header css

--- frontend/src/components/BaseLayout/BaseLayout.tsx
+++ frontend/src/components/BaseLayout/BaseLayout.tsx
@@ -1,17 +1,27 @@
 import { Outlet } from "react-router-dom";
 import classes from "./BaseLayout.module.css";
 import Navbar from "../Navbar/Navbar";
+import { Container } from "@mantine/core";
+import logo from '../../assets/logo.png';
 
 const BaseLayout = () => {
   return (
-    <div className={classes.layoutWrapper}>
+    <Container fluid className={classes.layoutWrapper}>
+      <div className={classes.headerContentWrapper}>
+        <div className={classes.logoImage}>
+          <img src={logo} alt="Centered" style={{ maxWidth: '100%', height: '100%' }} />
+        </div>
+        <div className={classes.headerContent}></div>
+      </div>
       <Navbar />
       <main className={classes.mainContentWrapper}>
         <div className={classes.mainContent}>
           <Outlet />
         </div>
       </main>
-    </div>
+
+    </Container>
+    
   );
 };
 
