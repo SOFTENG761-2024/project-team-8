f924744145120c2cee1d9b46f67d45e795552198
Tuan <tle428@aucklanduni.ac.nz>
2024-09-11 22:28:14 +1200

    style-header style with no logic

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,28 @@
 import { Container } from "@mantine/core"
 import classes from "./Header.module.css";
+import {
+  IconUserFilled
+} from "@tabler/icons-react";
+import explorer from '../../assets/explorer.png';
 
 const Header = () => {
   return (
     <Container className={classes.headerWrapper}>
-
+      <div className={classes.detailsWrapper}>
+      <img src={explorer} alt="Centered" style={{ maxWidth: '70px' }} />
+      <text className={classes.largeBoldText}>[Page Description]</text>
+      </div>
+      <div className={classes.detailsWrapper}>
+        <IconUserFilled size={50}/>
+        <div className={classes.nameWrapper}>
+          <text className={classes.text}>
+            Hello,
+          </text>
+          <text className={classes.boldText}>
+            Username
+          </text>
+        </div>
+      </div>
     </Container>
   )
 }
