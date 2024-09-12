638759b32714b1a737a27812cf7880fe7d01e8d3
Jolin <jche521@aucklanduni.ac.nz>
2024-09-13 10:32:44 +1200

    ✨ feat(create lesson modal): create layout for the modal

--- frontend/src/components/CreateLessonModal/CreateLessonModal.tsx
+++ frontend/src/components/CreateLessonModal/CreateLessonModal.tsx
@@ -17,9 +17,9 @@ const CreateLessonModal = () => {
   const [active, setActive] = useState(1);
   return (
     <>
-      <Modal opened={opened} onClose={close}>
+      <Modal opened={opened} onClose={close} size="100%" h="100%">
         <Flex>
-          <Stack>
+          <Stack h="100%" align="stretch" justify="flex-start" gap="md" w="30%">
             <Title size="h1" c="primary.4">
               Create a new lesson
             </Title>
@@ -27,6 +27,7 @@ const CreateLessonModal = () => {
               active={active}
               onStepClick={setActive}
               orientation="vertical"
+              size="xl"
             >
               <Stepper.Step label="Step 1" description="Add lesson details" />
               <Stepper.Step
@@ -36,24 +37,45 @@ const CreateLessonModal = () => {
               <Stepper.Step label="Step 3" description="Add lesson content" />
             </Stepper>
           </Stack>
-          {active === 0 && (
-            <Group>
-              <Divider orientation="vertical" />
-              <Text>Page 1</Text>
-            </Group>
-          )}
-          {active === 1 && (
-            <Group>
-              <Divider orientation="vertical" />
-              <Text>Page 2</Text>
-            </Group>
-          )}
-          {active === 2 && (
-            <Group>
-              <Divider orientation="vertical" />
-              <Text>Page 3</Text>
-            </Group>
-          )}
+          <Group w="70%" align="stretch" justify="flex-start" gap="md">
+            <Divider orientation="vertical" mx="lg" />
+            <Stack
+              h="100%"
+              w="80%"
+              align="stretch"
+              justify="flex-start"
+              gap="md"
+            >
+              {active === 0 && (
+                <>
+                  <Group w="100%">
+                    <Title size="h2" c="primary.4" w="100%">
+                      Lesson details
+                    </Title>
+                  </Group>
+
+                  <Divider />
+                  <Text>Page 1</Text>
+                </>
+              )}
+              {active === 1 && (
+                <>
+                  <Group>
+                    <Divider orientation="vertical" mx="lg" />
+                    <Text>Page 2</Text>
+                  </Group>
+                </>
+              )}
+              {active === 2 && (
+                <>
+                  <Group>
+                    <Divider orientation="vertical" mx="lg" />
+                    <Text>Page 3</Text>
+                  </Group>
+                </>
+              )}
+            </Stack>
+          </Group>
         </Flex>
       </Modal>
       <Button onClick={open}>Open create lesson modal</Button>
