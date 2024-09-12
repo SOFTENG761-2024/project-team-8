79ea240adbe5392dc527be179ab067e43b26c810
Manav Lal <manavlal5@gmail.com>
2024-09-12 12:37:45 +1200

    feat: added script to upload dino loops content

--- database-scripts/src/databaseUpload.ts
+++ database-scripts/src/databaseUpload.ts
@@ -1,6 +1,7 @@
 const path = require("path");
-const fs = require( "fs");
+const fs = require("fs");
 import Parse from 'parse/node';
+
 const dotenv = require('dotenv');
 
 dotenv.config(); // Loads variables from the .env file
@@ -9,43 +10,43 @@ const JS_KEY = process.env.VITE_BACK4APP_JS_KEY ?? "";
 Parse.initialize(APP_ID, JS_KEY);
 Parse.serverURL = "https://parseapi.back4app.com/";
 
- interface Kit {
+interface Kit {
     id: string;
     title: string;
     courses: Course[];
 
 }
 
- interface Course {
+interface Course {
     id: string;
     yearLevel: number;
-    description:string;
-    outcomes:string[];
+    description: string;
+    outcomes: string[];
     modules: string[]; // references to those objects
     assessments: Assessment[];
 }
 
 
- interface Module {
+interface Module {
     category: string;
     title: string;
     description: string;
     lessons: string[];
 }
 
- interface Assessment {
+interface Assessment {
     category: string;
     title: string;
-     printout: Parse.File[];
+    printout: Parse.File[];
 }
 
- interface Lesson {
+interface Lesson {
     id: string;
     title: string;
     content: Content[];
 }
 
- interface Content{
+interface Content {
     text: string;
     printout: Parse.File;
 }
@@ -72,8 +73,8 @@ async function createLesson(title: string, teacherHandoutPath: string, worksheet
     const lessonObj = new Parse.Object("lesson");
     lessonObj.set('title', title as any);
     const content: Content[] = [
-        { text: "Teacher_handout", printout: teacherHandout },
-        { text: "Worksheet", printout: worksheet }
+        {text: "Teacher_handout", printout: teacherHandout},
+        {text: "Worksheet", printout: worksheet}
     ];
     lessonObj.set('content', content as any);
 
@@ -89,19 +90,19 @@ async function uploadDinoSteps() {
     /* Upload lessons */
     const lessons = [
         {
-            title: "Lesson1",
-            handoutPath: path.join(__dirname,"../assets/dinoSteps/Demonstration copy - Dinosaur Steps Module - Lesson 01 - Teaching Slides.pdf"),
-            worksheetPath: path.join(__dirname,"../assets/dinoSteps/Demonstration copy - Dinosaur Steps Module - Student handouts - Lesson 01.pdf")
+            title: "DinoSteps - Lesson 1",
+            handoutPath: path.join(__dirname, "../assets/dinoSteps/Demonstration copy - Dinosaur Steps Module - Lesson 01 - Teaching Slides.pdf"),
+            worksheetPath: path.join(__dirname, "../assets/dinoSteps/Demonstration copy - Dinosaur Steps Module - Student handouts - Lesson 01.pdf")
         },
         {
-            title: "Lesson2",
-            handoutPath: path.join(__dirname,"../assets/dinoSteps/Demonstration copy -  Dinosaur Steps Module - Lesson 02 - Teaching Slides.pdf"),
-            worksheetPath: path.join(__dirname,"../assets/dinoSteps/Demonstration copy -  Dinosaur Steps Module - Student handouts - Lesson 02.pdf")
+            title: "DinoSteps - Lesson 2",
+            handoutPath: path.join(__dirname, "../assets/dinoSteps/Demonstration copy -  Dinosaur Steps Module - Lesson 02 - Teaching Slides.pdf"),
+            worksheetPath: path.join(__dirname, "../assets/dinoSteps/Demonstration copy -  Dinosaur Steps Module - Student handouts - Lesson 02.pdf")
         },
         {
-            title: "Lesson3",
-            handoutPath: path.join(__dirname,"../assets/dinoSteps/Demonstration copy - Dinosaur Steps Module - Lesson 03 - Teaching Slides.pdf"),
-            worksheetPath: path.join(__dirname,"../assets/dinoSteps/Demonstration Copy - Dinosaur Steps Module - Student handouts - Lesson 03.pdf")
+            title: "DinoSteps - Lesson 3",
+            handoutPath: path.join(__dirname, "../assets/dinoSteps/Demonstration copy - Dinosaur Steps Module - Lesson 03 - Teaching Slides.pdf"),
+            worksheetPath: path.join(__dirname, "../assets/dinoSteps/Demonstration Copy - Dinosaur Steps Module - Student handouts - Lesson 03.pdf")
         }
     ];
 
@@ -118,7 +119,7 @@ async function uploadDinoSteps() {
     }];
 
     /* Upload assessment */
-    const assessmentPath = path.join(__dirname,"../assets/dinoSteps/Demonstration copy - Dinosaur Steps formative assessment.pdf");
+    const assessmentPath = path.join(__dirname, "../assets/dinoSteps/Demonstration copy - Dinosaur Steps formative assessment.pdf");
     const dinoStepsAssessment = await uploadFile(assessmentPath);
 
     const assessment: Assessment = {
@@ -151,5 +152,70 @@ async function uploadDinoSteps() {
     }
 }
 
+async function uploadDinoLoops() {
+    /* Upload lessons */
+    const lessons = [
+        {
+            title: "DinoLoops - Lesson 1",
+            handoutPath: path.join(__dirname, "../assets/dinoLoops/Demonstration copy - Dinosaur Loops Module - Lesson 01 - Teaching Slides.pdf"),
+            worksheetPath: path.join(__dirname, "../assets/dinoLoops/Demonstration copy- Lesson 01 - Dinosaur Loops Module - Student handouts.pdf")
+        },
+        {
+            title: "DinoLoops - Lesson 2",
+            handoutPath: path.join(__dirname, "../assets/dinoLoops/Demonstration copy - Dinosaur Loops Module - Lesson 02 - Teaching Slides.pdf"),
+            worksheetPath: path.join(__dirname, "../assets/dinoLoops/Demonstration copy - Lesson 02 - Dinosaur Loops Module - Student handouts.pdf")
+        },
+        {
+            title: "DinoLoops - Lesson 3",
+            handoutPath: path.join(__dirname, "../assets/dinoLoops/Demonstration copy -  Dinosaur Loops Module - Lesson 03 - Teaching Slides.pdf"),
+            worksheetPath: path.join(__dirname, "../assets/dinoLoops/Demonstration copy - Lesson 03 -  Dinosaur Loops Module - Student handouts.pdf")
+        }
+    ];
+
+    const savedLessons = await Promise.all(lessons.map(lesson =>
+        createLesson(lesson.title, lesson.handoutPath, lesson.worksheetPath)
+    ));
+
+    /* Upload module */
+    const module: Module[] = [{
+        category: "Computational Thinking",
+        title: "Module 1",
+        lessons: savedLessons.map(lesson => lesson.id),
+        description: 'This module dives into computational thinking, algorithms and debugging.'
+    }];
+
+    /* Upload assessment */
+    const assessmentPath = path.join(__dirname, "../assets/dinoLoops/Demonstration copy - Dinosaur Loops formative assessment.pdf");
+    const dinoStepsAssessment = await uploadFile(assessmentPath);
+
+    const assessment: Assessment = {
+        category: "Computer Science",
+        title: "DinoLoops Assessment",
+        printout: [dinoStepsAssessment]
+    };
+
+    /* Create course */
+    const newCourse = new Parse.Object("course");
+    newCourse.set("yearLevel", 3 as any);
+    newCourse.set("description", 'To create content in programming, students need to develop a sound understanding of computational thinking (CT). In this module students will develop skills and knowledge to support their understanding of CT through unplugged and digital activities.' as any);
+    newCourse.set("outcomes", ["A", "B"] as any);
+    newCourse.set("modules", module as any);
+    newCourse.set("assessments", [assessment] as any);
+    const savedCourse = await newCourse.save();
+
+    /* Create kit */
+    const newKit = new Parse.Object("kits");
+    newKit.set("title", "Dinosaur Loops" as any);
+    newKit.set('courses', [savedCourse.id] as any);
+
+    try {
+        const result = await newKit.save();
+        console.log('Kit object created with objectId:', result.id);
+    } catch (error) {
+        console.error('Error while creating Kit object:', error);
+    }
+}
+
 
 uploadDinoSteps()
+uploadDinoLoops()
