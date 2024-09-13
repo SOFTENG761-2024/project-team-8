ab7a3e86cbdc9e14e70923280c4ab1866e950aab
Michelle <100409601+mlie788@users.noreply.github.com>
2024-09-14 00:19:34 +1200

    ♻️ refactor(coursedetails): added course summary component to course details page
    including small styling changes to seamlessly integrate

--- frontend/src/pages/Course.page.tsx
+++ frontend/src/pages/Course.page.tsx
@@ -1,21 +1,27 @@
 import { useParams } from "react-router-dom";
 import { Box, Group, Text, Title } from "@mantine/core";
 import CourseContent from "../components/Course/CourseContent";
+import { CourseSummary } from "../components/CourseSummary/CourseSummary.tsx";
 
 const CoursePage = () => {
   const { courseId } = useParams();
 
   return (
     <Box h="100%" w="100%">
-      <Title order={3} c="primary.5">Course Title <Text inherit span c="primary.3">- Dinosaur Steps</Text></Title>
-      <Group justify="space-between" align="top" h="100%">
-        <Box>
-          <Text>Placeholder for Course Summary</Text>
+      <Title order={3} c="primary.5">
+        Course Title{" "}
+        <Text inherit span c="primary.3">
+          - Dinosaur Steps
+        </Text>
+      </Title>
+      <Group justify="space-between" align="top" h="100%" mt="1rem">
+        <Box h="100%">
+          <CourseSummary />
         </Box>
         <CourseContent courseId={courseId} summaryExpanded />
       </Group>
     </Box>
-  )
+  );
 };
 
 export default CoursePage;
