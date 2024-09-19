730b38b7fd67c96007d8af2decea1a9ee67bf89c
tle428 <tle428@aucklanduni.ac.nz>
2024-09-19 19:13:00 +1200

    feat(component): added popup in module accordion

--- frontend/src/components/Course/ModuleAccordion.tsx
+++ frontend/src/components/Course/ModuleAccordion.tsx
@@ -1,6 +1,7 @@
-import { Accordion, Box, Group, Stack, Text, useMantineTheme } from "@mantine/core";
+import { Accordion, Box, Group, Modal, Stack, Text, useMantineTheme } from "@mantine/core";
 import { IconBook2, IconSquareRoundedChevronDownFilled, IconFile } from "@tabler/icons-react";
 import styles from "./ModuleAccordion.module.css";
+import { useState } from "react";
 
 interface ModuleAccordionProps {
     module: {
@@ -11,33 +12,54 @@ interface ModuleAccordionProps {
 
 const ModuleAccordion = ({ module }: ModuleAccordionProps) => {
     const theme = useMantineTheme();
+
+    const [opened, setOpened] = useState(false);
+    const [selectedLesson, setSelectedLesson] = useState<string | null>(null);
+
+    const handleLessonClick = (lesson: string) => {
+        setSelectedLesson(lesson);
+        setOpened(true);
+    };
+
     return (
-        <Accordion variant="separated" className={styles.accordionContainer} chevron={<IconSquareRoundedChevronDownFilled stroke={2.5} color={theme.colors.neutral[5]} />} chevronSize={20}>
-            <Accordion.Item key={module.title} value={module.title} className={styles.accordionItem}>
-                <Accordion.Control icon={<IconBook2 color={theme.colors.neutral[4]} />} c="neutral.5">
-                    <Group justify="space-between" pr="sm">
-                        <Text fw={700}>{module.title}</Text>
-                        <Text>{module.lessons.length} Lessons</Text>
-                    </Group>
-                </Accordion.Control>
-                <Accordion.Panel>
-                    <Stack>
-                        {module.lessons.map((lesson) => (
-                            <Box
-                                key={lesson}
-                                className={styles.lessonCard}
-                                p="1rem"
-                                >
-                                    <Group>
-                                        <IconFile />
-                                        <Text c="neutral.5">{lesson}</Text>
-                                    </Group>
-                            </Box>
-                        ))}
-                    </Stack>
-                </Accordion.Panel>
-            </Accordion.Item>
-        </Accordion>
+        <>
+            <Modal
+                opened={opened}
+                onClose={() => setOpened(false)}
+                title="Lesson Details"
+                centered
+            >
+                <Text>{selectedLesson}</Text>
+            </Modal>
+
+            <Accordion variant="separated" className={styles.accordionContainer} chevron={<IconSquareRoundedChevronDownFilled stroke={2.5} color={theme.colors.neutral[5]} />} chevronSize={20}>
+                <Accordion.Item key={module.title} value={module.title} className={styles.accordionItem}>
+                    <Accordion.Control icon={<IconBook2 color={theme.colors.neutral[4]} />} c="neutral.5">
+                        <Group justify="space-between" pr="sm">
+                            <Text fw={700}>{module.title}</Text>
+                            <Text>{module.lessons.length} Lessons</Text>
+                        </Group>
+                    </Accordion.Control>
+                    <Accordion.Panel>
+                        <Stack>
+                            {module.lessons.map((lesson) => (
+                                <Box
+                                    key={lesson}
+                                    className={styles.lessonCard}
+                                    p="1rem"
+                                    onClick={() => handleLessonClick(lesson)}
+                                    >
+                                        <Group>
+                                            <IconFile />
+                                            <Text c="neutral.5">{lesson}</Text>
+                                        </Group>
+                                </Box>
+                            ))}
+                        </Stack>
+                    </Accordion.Panel>
+                </Accordion.Item>
+            </Accordion>
+        </>
     )
 }
 
