6e5d636becda8293668558c6dd523e89d15ddf12
Atharva Arankalle <atharvanz@gmail.com>
2024-09-19 22:27:00 +1200

    ♻️ refactor(pdf-viewer): removed commented code and updated styles

--- frontend/src/components/PdfViewer/PdfViewer.tsx
+++ frontend/src/components/PdfViewer/PdfViewer.tsx
@@ -1,10 +1,14 @@
-import { Box, Stack } from "@mantine/core";
+import {
+  ActionIcon,
+  Box,
+  Button,
+  Group,
+  Stack,
+  Text,
+  Tooltip,
+} from "@mantine/core";
+import { IconMinimize } from "@tabler/icons-react";
 import styles from "./PdfViewer.module.css";
-// import {
-//   IconChevronDown,
-//   IconChevronUp,
-//   IconMinimize,
-// } from "@tabler/icons-react";
 import { useState } from "react";
 import { Document, Page } from "react-pdf";
 import { pdfjs } from "react-pdf";
@@ -20,6 +24,10 @@ interface PdfViewerProps {
   setFullscreen: (fullscreen: boolean) => void;
 }
 
+interface OnLoadSuccessProps {
+  numPages: number;
+}
+
 /**
  * This component handles the logic for displaying a PDF file on the page, using the default browser PDF viewer.
  * It also provides a fullscreen mode to view the PDF file.
@@ -29,58 +37,28 @@ interface PdfViewerProps {
  * @param {function} setFullscreen - A function to set the fullscreen mode of the viewer.
  */
 const PdfViewer = ({ url, fullscreen, setFullscreen }: PdfViewerProps) => {
-  // const [barMinimized, setBarMinimized] = useState<boolean>(false);
-  const [numPages, setNumPages] = useState<number>();
+  const [numPages, setNumPages] = useState<number>(1);
   const [pageNumber, setPageNumber] = useState<number>(1);
 
-  // const minimizePdf = (event: React.MouseEvent<HTMLButtonElement>) => {
-  //   event.stopPropagation();
-  //   setFullscreen(false);
-  // };
-
-  // const toggleBar = () => {
-  //   setBarMinimized(!barMinimized);
-  // };
-
+  const minimizePdf = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    setFullscreen(false);
+  };
   console.log(setFullscreen);
 
-  function onDocumentLoadSuccess({ numPages: nextNumPages }): void {
-    setNumPages(nextNumPages);
+  function onDocumentLoadSuccess({ numPages }: OnLoadSuccessProps): void {
+    setNumPages(numPages);
     setPageNumber(1);
   }
 
-  function changePage(offset) {
+  function changePage(offset: number) {
     setPageNumber((prevPageNumber) => prevPageNumber + offset);
-  }
-
-  function previousPage() {
-    changePage(-1);
-  }
-
-  function nextPage() {
-    changePage(1);
   }
 
   return (
     <Box className={fullscreen ? styles.fullscreenContainer : ""}>
       <Stack>
-        <div>
-          <p>
-            Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
-          </p>
-          <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
-            Previous
-          </button>
-          <button
-            type="button"
-            disabled={pageNumber >= numPages}
-            onClick={nextPage}
-          >
-            Next
-          </button>
-        </div>
         <Box style={{ display: "flex", justifyContent: "center" }}>
-          {/* TODO: Add navbar type element here to display page number, navigation buttons and fullscreen button then won't need stack */}
           <Document
             file={url}
             onLoadSuccess={onDocumentLoadSuccess}
@@ -98,51 +76,45 @@ const PdfViewer = ({ url, fullscreen, setFullscreen }: PdfViewerProps) => {
             />
           </Document>
         </Box>
+        <Box
+          display="flex"
+          className={styles.navigationMenu}
+          bg={fullscreen ? "primary.2" : "transparent"}
+          w={fullscreen ? "100%" : "auto"}
+          h={fullscreen ? "7vh" : "auto"}
+          style={{ position: fullscreen ? "absolute" : "relative", top: 0 }}
+        >
+          <Group gap="2rem" m="0 auto">
+            <Button
+              onClick={() => changePage(-1)}
+              disabled={pageNumber <= 1}
+              size="md"
+            >
+              Previous
+            </Button>
+            <Text size="1.5rem">
+              Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+            </Text>
+            <Button
+              onClick={() => changePage(1)}
+              disabled={pageNumber >= numPages}
+              size="md"
+            >
+              Next
+            </Button>
+          </Group>
+          {fullscreen && (
+            <Box>
+              <Tooltip label="Exit Fullscreen">
+                <ActionIcon onClick={minimizePdf} size={40}>
+                  <IconMinimize size={40} className={styles.minimizeIcon} />
+                </ActionIcon>
+              </Tooltip>
+            </Box>
+          )}
+        </Box>
       </Stack>
     </Box>
-    // <Box className={fullscreen ? styles.fullscreenContainer : ""}>
-    //   {fullscreen && (
-    //     <Box
-    //       className={styles.fullscreenTopMenu}
-    //       bg="primary.5"
-    //       onClick={toggleBar}
-    //       style={{
-    //         height: barMinimized ? "1vh" : "5vh",
-    //         transition: "height ease-out 0.2s",
-    //       }}
-    //     >
-    //       {barMinimized ? (
-    //         <Tooltip label="Expand">
-    //           <IconChevronDown color="white" style={{ marginLeft: "50vw" }} />
-    //         </Tooltip>
-    //       ) : (
-    //         <>
-    //           <Tooltip label="Hide">
-    //             <IconChevronUp color="white" style={{ marginLeft: "50vw" }} />
-    //           </Tooltip>
-    //           <Tooltip label="Exit Fullscreen">
-    //             <ActionIcon onClick={minimizePdf} size={30}>
-    //               <IconMinimize size={40} className={styles.minimizeIcon} />
-    //             </ActionIcon>
-    //           </Tooltip>
-    //         </>
-    //       )}
-    //     </Box>
-    //   )}
-    //   <Center>
-    //     <object
-    //       data={url + "#view=Fit"}
-    //       type="text/html"
-    //       className={
-    //         fullscreen ? styles.fullscreenView : styles.nonFullscreenView
-    //       }
-    //       style={{
-    //         height: fullscreen ? (barMinimized ? "99vh" : "95vh") : "50vh",
-    //         transition: "height ease-out 0.2s",
-    //       }}
-    //     ></object>
-    //   </Center>
-    // </Box>
   );
 };
 
