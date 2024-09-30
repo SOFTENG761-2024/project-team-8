479e993bddb2ef8dbdfd6c8add0eee603c61a930
Michelle <100409601+mlie788@users.noreply.github.com>
2024-09-30 23:28:29 +1300

    ✨ feat(pages): applied page title changes to all pages
    Additionally created a separated constants file for page titles

--- frontend/src/pages/Welcome.page.tsx
+++ frontend/src/pages/Welcome.page.tsx
@@ -1,11 +1,23 @@
 import UserLogin from "../components/Authentication/UserLogin";
 import UserRegister from "../components/Authentication/UserRegister";
 import WelcomeContainer from "../components/WelcomeContainer";
+import { useEffect } from "react";
+import { formattedPageTitle } from "../constants/pageTitles.ts";
 
 interface WelcomePagePropTypes {
   state: "login" | "register";
 }
 const WelcomePage = ({ state }: WelcomePagePropTypes) => {
+  const path = location.pathname;
+
+  useEffect(() => {
+    if (path.includes("login")) {
+      document.title = formattedPageTitle("LOGIN");
+    } else if (path.includes("register")) {
+      document.title = formattedPageTitle("REGISTER");
+    }
+  }, [path]);
+
   return (
     <>
       {state === "login" && (
