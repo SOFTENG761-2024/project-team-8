f7fc8b5ec6cc724376119f4ba0f1045b25c02b4e
Atharva Arankalle <atharvanz@gmail.com>
2024-10-05 19:42:30 +1300

    ✨ feat(lesson-integration): integrated viewer with lesson overlay and updated style

--- frontend/src/components/Lesson/LessonContentSection.tsx
+++ frontend/src/components/Lesson/LessonContentSection.tsx
@@ -1,5 +1,7 @@
 import { Group, Button, Stack, Title, Text, Divider } from "@mantine/core";
 import { IconArrowsMaximize, IconDownload } from "@tabler/icons-react";
+import PdfViewer from "../PdfViewer/PdfViewer";
+import { useState } from "react";
 
 interface LessonContentSectionProps {
   title: string;
@@ -11,29 +13,43 @@ const LessonContentSection = ({
   title,
   description,
   fileUrl,
-}: LessonContentSectionProps) => (
-  <Stack py="0.5rem">
-    <Divider />
+}: LessonContentSectionProps) => {
+  const [fullscreen, setFullscreen] = useState<boolean>(false);
 
-    {/* Content title and description */}
-    <Title size="h4" fw="700" c="primary.5" pt="1rem">
-      {title}
-    </Title>
-    <Text ta="justify"> {description}</Text>
-    {/* PDF button group */}
-    <Group justify="center" gap="1rem" py="0.5rem">
-      <Button variant="filled" leftSection={<IconDownload size="1.25rem" />}>
-        Download File
-      </Button>
-      <Button
-        variant="filled"
-        leftSection={<IconArrowsMaximize size="1.25rem" />}
-      >
-        View Fullscreen
-      </Button>
-    </Group>
-    <Text ta="center">(Add PDF viewer here using: {fileUrl})</Text>
-  </Stack>
-);
+  return (
+    <Stack py="0.5rem">
+      <Divider />
+
+      {/* Content title and description */}
+      <Title size="h4" fw="700" c="primary.5" pt="1rem">
+        {title}
+      </Title>
+      <Text ta="justify"> {description}</Text>
+      {/* PDF button group */}
+      <Group justify="center" gap="1rem" py="0.5rem">
+        <a target="_blank" href={fileUrl}>
+          <Button
+            variant="filled"
+            leftSection={<IconDownload size="1.25rem" />}
+          >
+            Download File
+          </Button>
+        </a>
+        <Button
+          variant="filled"
+          leftSection={<IconArrowsMaximize size="1.25rem" />}
+          onClick={() => setFullscreen(true)}
+        >
+          View Fullscreen
+        </Button>
+      </Group>
+      <PdfViewer
+        url={fileUrl}
+        fullscreen={fullscreen}
+        setFullscreen={setFullscreen}
+      />
+    </Stack>
+  );
+};
 
 export default LessonContentSection;
