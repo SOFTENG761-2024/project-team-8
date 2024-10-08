79b7f3caa67fc9e09cfe9ff617212beb0ec13a1c
Jolin <jche521@aucklanduni.ac.nz>
2024-10-08 21:42:40 +1300

    💎 style: remove white spaces and adjusting size of header

--- frontend/src/components/BaseLayout/BaseLayout.tsx
+++ frontend/src/components/BaseLayout/BaseLayout.tsx
@@ -2,29 +2,26 @@ import { Outlet } from "react-router-dom";
 import classes from "./BaseLayout.module.css";
 import Navbar from "../Navbar/Navbar";
 import Header from "../Header/Header";
-import { Box } from "@mantine/core";
+import { Box, Divider, Image } from "@mantine/core";
 import logo from "../../assets/logo.png";
-import Footer from "../Footer/Footer";
 
 const BaseLayout = () => {
   return (
     <Box className={classes.layoutWrapper}>
       <div className={classes.headerContentWrapper}>
         <div className={classes.logoImage}>
-          <img src={logo} alt="logo" className={classes.logo} />
+          <Image src={logo} alt="ByteEd" h={120} />
         </div>
         <div className={classes.headerContent}>
           <Header />
         </div>
       </div>
+      <div className={classes.divider} />
       <div className={classes.horizontalWrapper}>
         <Navbar />
         <main className={classes.mainContentWrapper}>
           <Outlet />
         </main>
-      </div>
-      <div className={classes.footerWrapper}>
-        <Footer />
       </div>
     </Box>
   );
